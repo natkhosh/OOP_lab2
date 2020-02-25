bfec49ce81f8a040806d696845ddfea7bb881981
natkhosh <t9219788607@gmail.com>
2020-02-25 09:29:21 +0300

    Lab2 -->  start

--- painter.py
+++ painter.py
@@ -1,12 +1,13 @@
 import os
 
-from figures import Figure, Rectangle, Ellipse, CloseFigure
-
 import sys
 # Подключаем модули QApplication и QLabel
 from PySide2.QtWidgets import QApplication, QWidget
 from PySide2.QtGui import QPainter, QBrush
 from PySide2.QtCore import Qt, QPoint
+
+# Импортируйте свой файл с фигурами
+from figures import Figure, Rectangle
 
 
 class FigureWidget(QWidget):
@@ -19,7 +20,6 @@ class FigureWidget(QWidget):
         self.__figures = figures
 
     def paintEvent(self, event):
-
         painter = QPainter(self)
         reset_brush = painter.brush()
 
@@ -28,14 +28,15 @@ class FigureWidget(QWidget):
                 continue
 
             if isinstance(figure, Rectangle):
-                painter.setBrush(QBrush(Qt.magenta))
-                painter.drawRect(figure.x, figure.y, figure.width, figure.height)
+                painter.setBrush(QBrush(Qt.red))
+                painter.drawRect(figure.x, figure.y,
+                                 figure.width, figure.height)
                 continue
 
-            if isinstance(figure, Ellipse):
-                painter.setBrush(QBrush(Qt.green))
-                painter.drawEllipse(figure.x, figure.y, figure.width, figure.height)
-                continue
+            # if isinstance(figure, Ellipse):
+            #     painter.setBrush(QBrush(Qt.green))
+            #     painter.drawEllipse(figure.x(), figure.y(), figure.width(), figure.height())
+            #     continue
 
             if isinstance(figure, CloseFigure):
                 painter.setBrush(QBrush(Qt.blue))
@@ -53,10 +54,10 @@ if __name__ == '__main__':
     figure_widget = FigureWidget()
 
     # Создайте список фигур
+    figures = [Rectangle(20, 30, 400, 200),
+               Rectangle(100, 300, 300, 50), CloseFiruse([(), ()), ()])]
+
     # figures = []
-    # figures = [CloseFigure((50, 50), (250, 100), (350, 350), (200, 400), (30, 380), (100, 130), (50, 50))]
-    figures = [Rectangle(10, 10, 200, 100), Rectangle(250, 400, 300, 50), Ellipse(220, 30, 400, 200), Ellipse(340, 300, 300, 50),
-               CloseFigure((50, 50), (250, 100), (350, 350), (200, 400), (30, 380), (100, 130), (50, 50))]
     figure_widget.set_figures(figures)
 
     figure_widget.show()
